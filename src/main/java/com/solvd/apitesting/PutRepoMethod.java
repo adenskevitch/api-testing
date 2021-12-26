26b494905eb101e8f3ce187985fedc39d9598dcf
AlexanderDenskevitch <84723495+AlexanderDenskevitch@users.noreply.github.com>
2021-12-26 13:21:35 +0300

    Five API requests was added.

--- src/main/java/com/solvd/apitesting/PutRepoMethod.java
+++ src/main/java/com/solvd/apitesting/PutRepoMethod.java
@@ -3,10 +3,10 @@ package com.solvd.apitesting;
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
-public class GetRepoMethod extends AbstractApiMethodV2 {
+public class PutRepoMethod extends AbstractApiMethodV2 {
 
-    public GetRepoMethod() {
-        super(null, "api/repos/_get/rs.json", "api/repos/repos.properties");
+    public PutRepoMethod() {
+        super("api/repos/_put/rq.json", null, "api/repos/repos.properties");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
     }
 }
--- src/test/java/com/solvd/apitesting/TestAPI.java
+++ src/test/java/com/solvd/apitesting/TestAPI.java
@@ -3,13 +3,53 @@ package com.solvd.apitesting;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import org.testng.annotations.Test;
 
+import java.util.Base64;
+
 public class TestAPI {
 
-    @Test()
-    public void testGetRepos() {
+    @Test
+    public void testPostRepo() {
+        PostRepoMethod postRepoMethod = new PostRepoMethod();
+        postRepoMethod.setHeaders(
+                String.format("Authorization=%s", "token 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"));
+        postRepoMethod.expectResponseStatus(HttpResponseStatusType.CREATED_201);
+        postRepoMethod.callAPI();
+    }
+
+    @Test(dependsOnMethods = "testPostRepo")
+    public void testPutRepo() {
+        PutRepoMethod putRepoMethod = new PutRepoMethod();
+        putRepoMethod.setHeaders(
+                String.format("Authorization=%s", "token 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"));
+        String testText = "Test text";
+        String encodeTestText = Base64.getEncoder().encodeToString(testText.getBytes());
+        putRepoMethod.addProperty("content", encodeTestText);
+        putRepoMethod.expectResponseStatus(HttpResponseStatusType.CREATED_201);
+        putRepoMethod.callAPI();
+    }
+
+    @Test(dependsOnMethods = "testPutRepo")
+    public void testGetRepo() {
         GetRepoMethod getRepoMethod = new GetRepoMethod();
         getRepoMethod.expectResponseStatus(HttpResponseStatusType.OK_200);
         getRepoMethod.callAPI();
-        getRepoMethod.validateResponse();
+    }
+
+    @Test(dependsOnMethods = "testGetRepo")
+    public void testPatchRepo() {
+        PatchRepoMethod patchRepoMethod = new PatchRepoMethod();
+        patchRepoMethod.setHeaders(
+                String.format("Authorization=%s", "token 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"));
+        patchRepoMethod.expectResponseStatus(HttpResponseStatusType.OK_200);
+        patchRepoMethod.callAPI();
+    }
+
+    @Test(dependsOnMethods = "testPatchRepo")
+    public void testDeleteRepo() {
+        DeleteRepoMethod deleteRepoMethod = new DeleteRepoMethod();
+        deleteRepoMethod.setHeaders(
+                String.format("Authorization=%s", "token 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"));
+        deleteRepoMethod.expectResponseStatus(HttpResponseStatusType.NO_CONTENT_204);
+        deleteRepoMethod.callAPI();
     }
 }
